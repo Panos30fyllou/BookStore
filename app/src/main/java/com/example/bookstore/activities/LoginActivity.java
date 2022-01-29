6873390b42646924d197e5b96a9edfd4fe50a101
Panos Triantafyllou <panos30fyllou@gmail.com>
2022-01-29 16:46:19 +0200

    Connected to Firebase

--- app/src/main/java/com/example/bookstore/activities/LoginActivity.java
+++ app/src/main/java/com/example/bookstore/activities/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.bookstore.activities;
 
+import com.example.bookstore.R;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText username = findViewById(R.id.usernameEditText);
+        EditText password = findViewById(R.id.passwordEditText);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+
+
+
     }
 }
--- app/src/main/java/com/example/bookstore/activities/RegisterActivity.java
+++ app/src/main/java/com/example/bookstore/activities/RegisterActivity.java
@@ -1,14 +1,20 @@
 package com.example.bookstore.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.bookstore.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    DatabaseReference db = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
     }
 }
